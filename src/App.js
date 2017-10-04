f609c84a9c003630b1f7040d3145bd9b8c4f1560
Carlos Filoteo <c.filoteo@live.com>
2017-10-03 23:28:12 -0600

    Add basic state to toggle first song of data source

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
+import data from './data';
 
 class App extends Component {
+  state = {
+    src: data[0].link
+  }
+  togglePlay = () => {
+    this.audio[this.audio.paused ? 'play' : 'pause']();
+	}
   render() {
     return (
       <div className="App">
+        <audio 
+          ref={(a) => { this.audio = a }} 
+          src={this.state.src} ></audio>
+        <button type="button" onClick={this.togglePlay}>Play/pause</button>
       </div>
     );
   }
